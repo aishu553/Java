93c2e7352fca415552a899ca91443947982c0fe5
aishu553 <aishwarya89701@gmail.com>
2026-07-19 22:46:00 +0530

    Implement Cycle Sort algorithm in CycleSort.java

---
+++ Sorting/CycleSort.java
@@ -0,0 +1,20 @@
+static void cycleSort(int[] arr){
+
+    int i=0;
+
+    while(i<arr.length){
+
+        int correct=arr[i]-1;
+
+        if(arr[i]!=arr[correct]){
+
+            int temp=arr[i];
+            arr[i]=arr[correct];
+            arr[correct]=temp;
+        }
+
+        else{
+            i++;
+        }
+    }
+}
