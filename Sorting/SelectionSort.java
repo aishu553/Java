37c86d44c4591d0da5ad9a3663187d7cb0479285
aishu553 <aishwarya89701@gmail.com>
2026-07-19 10:42:29 +0530

    Correct selection sort implementation
    Fixed loop conditions and improved output formatting.

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -2,21 +2,22 @@
 
 import java.util.*;
 
-public class Main{
-  public static void main(String[] args){
-    int[] arr={ 7, 3, 8, 2, 6, 4, 5};
-    int n=arr.length;
-    for(int i=0;i<n-1;i++){
-      int min=i;
-      for(int j=i+1;i<n;i++){
-        if(arr[min]>arr[j]){
-          min=j;
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {7, 3, 8, 2, 6, 4, 5};
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
         }
-      }
-      arr[i]=arr[min];
+        for (int x : arr)
+            System.out.print(x + " ");
     }
-    for(int x:arr){
-      System.out.print(x+" ");
-    }
-  }
 }
